433e11a8fedcc802755815396627664ad2d7f02f
TechSolanki <mradulpratapsolanki@gmail.com>
2026-06-10 07:40:37 +0530

    Create build.gradle.kts

---
+++ AllPronStream/build.gradle.kts
@@ -0,0 +1,11 @@
+// ! techmind09 // @cloudstream-nsfw indian // 
+version = 1
+
+cloudstream {
+    authors     = listOf("techmind09")
+    language    = "en"
+    description = "All Porn Stream - Free Porn HD Videos. Watch the best high definition sex movies with hot babes. Allpornstream.com provides you thousands of 1080p, 4k and free porn videos, hd porn tube"
+    status  = 1 // will be 3 if unspecified
+    tvTypes = listOf("NSFW")
+    iconUrl = "https://t3.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://allpornstream.com/&size=128"
+}
